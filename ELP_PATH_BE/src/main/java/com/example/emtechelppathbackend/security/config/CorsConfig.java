0e949f25c4d10f6f476f59945d7d7cd38a330663
Jepkosgei3 <95687538+Jepkosgei3@users.noreply.github.com>
2025-01-20 09:05:57 +0300

    Update CorsConfig.java

--- ELP_PATH_BE/src/main/java/com/example/emtechelppathbackend/security/config/CorsConfig.java
+++ ELP_PATH_BE/src/main/java/com/example/emtechelppathbackend/security/config/CorsConfig.java
@@ -17,10 +17,10 @@ public class CorsConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Collections.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin: *", "Access-Control-Allow-Credentials: Origin, Content-Type, X-Auth-Token, Authorization, Accept"));
-        configuration.setAllowedHeaders(Collections.singletonList("*")); //deploying
+        configuration.setAllowedHeaders(Collections.asList("*")); //deploying
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
